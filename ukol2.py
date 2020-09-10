c146256813fb344697a0f8f17f89ccc411e63096
Vaclav.Vachuska <39367831+VaclavVachuska@users.noreply.github.com>
2020-09-10 09:07:40 +0200

    Update ukol2.py

--- ukol2.py
+++ ukol2.py
@@ -1,4 +1,9 @@
 a = 0
-while a < 10: 
-    print(a)
-    a += 1
+
+while a < 10:
+    if (a % 2) == 0: 
+        print(a, "je sudé")
+        a += 1
+    else:
+        print(a, "je liché")
+        a +=1
