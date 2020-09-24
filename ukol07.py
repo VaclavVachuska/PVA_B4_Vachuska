24283f3bcf9e14cca17fc61e3c91b76dc9b2ba50
Vaclav.Vachuska <39367831+VaclavVachuska@users.noreply.github.com>
2020-09-24 09:06:22 +0200

    Update ukol07.py

--- ukol07.py
+++ ukol07.py
@@ -4,9 +4,12 @@ a = 1
 def my_function():
   print("|", " " * (a+2), "|")
 
-for a in range(1,i+1):
-  print(" /", "=" * a, "\\")
-  for y in range(1,a):
-    my_function()
-  print(" \\", "=" * a, "/","\n")
-  a += 1
+if i in range(2,21):
+  for a in range(1,i+1):
+    print(" /", "=" * a, "\\")
+    for y in range(1,a):
+      my_function()
+    print(" \\", "=" * a, "/","\n")
+    a += 1
+else:
+  print("Zadali jste špatnou hodnotu!")  
