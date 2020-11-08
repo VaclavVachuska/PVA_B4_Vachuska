a49eb13b6139f8799eb71f185943128bb4064a1e
Vaclav.Vachuska <39367831+VaclavVachuska@users.noreply.github.com>
2020-11-08 18:44:37 +0100

    Create ukol13.py

---
+++ ukol13.py
@@ -0,0 +1,15 @@
+import sys
+
+print('<html>\n<head>\n<style>\ntable, th, td {\n border: 1px solid black;\n}\n</style>\n</head>\n<body>\n<table style="width:100%">')
+
+for line in sys.stdin:
+
+    if line == "":
+        continue
+    line = line.split(";")
+    print("<tr>")
+    for i in line:
+        print('<th>', i, '</th>')
+    print("</tr>")
+
+print('</table>\n</body>\n</html>')
