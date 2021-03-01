7b9b1defef6b5e591bdef1779db25a2a2402b5df
Vaclav.Vachuska <39367831+VaclavVachuska@users.noreply.github.com>
2021-03-01 21:13:12 +0100

    Update kalkulacka.py

--- kalkulacka.py
+++ kalkulacka.py
@@ -1,7 +1,5 @@
-import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.core.window import Window
 
